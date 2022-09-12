eaaf7583b0901d69b2c2523842db7bce5fab651e
hustnmt <manhtung05072001@gmail.com>
2022-09-12 16:32:49 +0700

    btl

--- src/main/java/com/test/nmt/model/ticket/TicketDetailByUserDTO.java
+++ src/main/java/com/test/nmt/model/ticket/TicketDetailByUserDTO.java
@@ -1,5 +1,9 @@
 package com.test.nmt.model.ticket;
 
+import java.util.List;
+
+import com.test.nmt.model.scheduleRequest.MovieScheduleRequestDTO;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
